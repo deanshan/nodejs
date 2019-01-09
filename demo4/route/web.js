eaff3c8ea9ae50dd5e50a36ee3f050a9d3c863b8
shandean <deanshanm@163.com>
2019-01-09 18:57:46 +0800

    io

--- demo4/route/web.js
+++ demo4/route/web.js
@@ -2,15 +2,17 @@ const express=require('express')
 const fs = require('fs')
 const marked = require('marked')
 
-let data
-let path = "es6.md"
-fs.readFile(path, function(err, data) {
-  if(err) {
-      throw err
-  } else {
-      data = marked(data.toString())
-  }
-})
+let data = {
+  text = "1111"
+}
+// let path = "es6.md"
+// fs.readFile(path, function(err, data) {
+//   if(err) {
+//       throw err
+//   } else {
+//       data = marked(data.toString())
+//   }
+// })
 
 module.exports = (function() {
   let router = express.Router();
